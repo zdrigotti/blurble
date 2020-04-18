a650c9e24885326dfc485f368bf8b90baaa3f90e
Zach Rigotti <zdrigotti@gmail.com>
2020-04-18 13:15:04 -0700

    Auth pipeline

---
+++ lib/blurble/user_manager/pipeline.ex
@@ -0,0 +1,13 @@
+defmodule Blurble.UserManager.Pipeline do
+  use Guardian.Plug.Pipeline,
+    otp_app: :blurble,
+    error_handler: Blurble.UserManager.ErrorHandler,
+    module: Blurble.UserManager.Guardian
+
+  # If there is a session token, restrict it to an access token and validate it
+  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+  # If there is an authorization header, restrict it to an access token and validate it
+  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+  # Load the user if either of the verifications worked
+  plug Guardian.Plug.LoadResource, allow_blank: true
+end
