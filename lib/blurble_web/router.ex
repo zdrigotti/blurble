c1414c260a470d3626ca73c6cc073324b849f364
Zach Rigotti <zdrigotti@gmail.com>
2020-04-18 13:17:54 -0700

    Routes and templates for login/logout and sample protected route

--- lib/blurble_web/router.ex
+++ lib/blurble_web/router.ex
@@ -13,14 +13,27 @@ defmodule BlurbleWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", BlurbleWeb do
-    pipe_through :browser
-
-    get "/", PageController, :index
+  pipeline :auth do
+    plug Blurble.UserManager.Pipeline
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", BlurbleWeb do
-  #   pipe_through :api
-  # end
+  pipeline :ensure_auth do
+    plug Guardian.Plug.EnsureAuthenticated
+  end
+
+  scope "/", BlurbleWeb do
+    pipe_through [:browser, :auth]
+
+    get "/", PageController, :index
+
+    get "/login", SessionController, :new
+    post "/login", SessionController, :login
+    get "/logout", SessionController, :logout
+  end
+
+  scope "/", BlurbleWeb do
+    pipe_through [:browser, :auth, :ensure_auth]
+
+    get "/protected", PageController, :protected
+  end
 end
---
+++ lib/blurble_web/views/session_view.ex
@@ -0,0 +1,3 @@
+defmodule BlurbleWeb.SessionView do
+  use BlurbleWeb, :view
+end
